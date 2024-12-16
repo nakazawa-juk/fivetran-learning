f05e607601d53b63ea629c5d3e139b21e0de98ec
nakazawa-juk <122271730+nakazawa-juk@users.noreply.github.com>
2024-12-16 09:31:14 +0900

    fix

--- addConnector.sh
+++ addConnector.sh
@@ -1,6 +1,6 @@
 # FivetranのAPIキーとシークレットを設定
-API_KEY="B27zjJv9FKoWnegi"
-API_SECRET_KEY="jnsoascLcUzDBUDkUW60uGg8pD1edvoc"
+API_KEY="xxxx"
+API_SECRET_KEY="yyyy"
 
 # APIエンドポイント
 URL="https://api.fivetran.com/v1/connectors"
@@ -14,11 +14,11 @@ BODY='{
   	},
     "auth": {
         "client_access": {
-            "client_id": "4152769596-den4paj21oq7r72ccs9gioh7eb1pinq9.apps.googleusercontent.com",
-            "client_secret": "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"
+            "client_id": "xxxx.apps.googleusercontent.com",
+            "client_secret": "xxxx"
         },
-        "refresh_token": "1//0eebM6P09H_UhCgYIARAAGA4SNwF-L9IrHws_9f3mipaIU2c43uHjaLzA7neee5M4Cs9Pug6kkUbX1Q4YyiOiW_jNX9KKdHZz-3c",
-        "access_token": "ya29.a0AcM612x58UFuDqxk26_7u-lOunAhn4ss5cj9J3xU7qIqNaQPCu7xqaIPlUfYngO1_yk8I5e-2TBLuKYxUFiMuchYKCMbH7wtHSJcP_0zqIEWyhh9tvgSON_e-ijsMF25iX1JILNXIiNhz8T0A3pFeqpBmrfAd-uQhIR4EU9BaCgYKAQYSARASFQHGX2MiJur7x5cBCaPMuVCrUwuMqA0175"
+        "refresh_token": "xxxx",
+        "access_token": "xxxx"
     }
 }'
 
